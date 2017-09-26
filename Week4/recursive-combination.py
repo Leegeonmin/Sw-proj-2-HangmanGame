2f786703ef1389c3575fe7badf2c76f96e0c41da
LEE GEON MIN <lkm9709@kookmin.ac.kr>
2017-09-26 11:09:31 +0900

    add (n-k)loop

--- Week4/recursive-combination.py
+++ Week4/recursive-combination.py
@@ -1,13 +1,16 @@
 # 20171660이건민
-
-def recursive_combination(n, k):    # nCk에서 n,k를 입력받는다
-        if not(type(n or k)) == int:    #type이 정수가 아니면 끝냄
+import time
+# nCk에서 n,k를 입력받는다
+def recursive_combination(n, k):
+         # type이 정수가 아니면 다시 입력받음
+        if not(type(n or k)) == int:
             print("Enter Integer")
             n = int(input("Enter integer n:"))
             k = int(input("Enter integer k:"))
             Re = recursive_combination(n, k)
             print(Re)
-        elif ((n or k) < 0) or (n < k):           # 예외 처리
+        # 예외처리
+        elif ((n or k) < 0) or (n < k):
             print("Enter n,k(n,k > 0 and n < k)")
             n = int(input("Enter integer n:"))
             k = int(input("Enter integer k:"))
@@ -15,18 +18,41 @@ def recursive_combination(n, k):    # nCk에서 n,k를 입력받는다
             print(Re)
 
         else:
-            if k == 1:  # 함수가 k를 1로 입력받을 시 nC1 = n 을 이용한다
-                return n
-            elif n == k:  # n = k 일시 nCk = 1
-             return 1
-            elif k == 0:    # k = 0 일시 nCk = 1
-             return 1
+        # 처리 시간을 줄이기 위해 nCk에서 k가 클 경우 변환해준다 ex) 5C4 = 5C1
+            if n - k >= k:
+                # 함수가 k를 1로 입력받을 시 nC1 = n 을 이용한다
+                if k == 1:
+                    return n
+             # n = k 일시 nCk = 1
+                elif n == k:
+                    return 1
+             # k = 0 일시 nCk = 1
+                elif k == 0:
+                    return 1
+                else:
+                    return recursive_combination(n - 1, k - 1) + recursive_combination(n - 1, k)
+                    print(recursive_combination(n - 1, k - 1) + recursive_combination(n - 1, k))
             else:
-                return recursive_combination(n - 1, k - 1) + recursive_combination(n - 1, k)
-                print(recursive_combination(n - 1, k - 1) + recursive_combination(n - 1, k))
-
-
+                k = n-k
+                # 함수가 k를 1로 입력받을 시 nC1 = n 을 이용한다
+                if k == 1:
+                    return n
+                # n = k 일시 nCk = 1
+                elif n == k:
+                    return 1
+                # k = 0 일시 nCk = 1
+                elif k == 0:
+                    return 1
+                else:
+                    return recursive_combination(n - 1, k - 1) + recursive_combination(n - 1, k)
+                    print(recursive_combination(n - 1, k - 1) + recursive_combination(n - 1, k))
 
 __name__=="__main__"
-N = recursive_combination("a",2)
+a = time.time()
+N = recursive_combination(25, 15)
 print(N)
+b = time.time()
+print(b - a)
+
+
+### 처리시간을 줄이기 위해 만든 if, else 문을 이용해 25C15를 실행해본결과 약 2배(1.7초 , 0.8초)차이가
