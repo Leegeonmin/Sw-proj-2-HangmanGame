fcb89cbb45d4bab9feac49040f41190983ba4ec7
LEE GEON MIN <lkm9709@kookmin.ac.kr>
2017-10-08 23:19:36 +0900

    add fibo-iterative file

---
+++ Week5/20171660이건민.py
@@ -0,0 +1,49 @@
+## 20171660 이건민
+## 4분반
+## 피보나치 수열을 반복적으로 구현
+
+import time
+
+## fibo라는 리스트를 만들고
+## fibo(0), fibo(1) 값을 0 , 1로 지정
+fibolist = []
+fibolist.append(0)
+fibolist.append(1)
+
+
+def iterfibo(n):
+    ## 피보나치 수열의 합을 저장할 sum = 0 지정
+    sum = 0
+    ## 피보나치 수열을 fibo리스트에 저장
+    for i in range(2, n):
+        fibolist.append(fibolist[i-1] + fibolist[i-2])
+    ## fibo리스트의 값들을 합
+    for p in range(n-2, n):
+        sum += fibolist[p]
+    return sum
+
+
+def fibo(n):
+	if n <= 1:
+		return n
+	return fibo(n - 1) + fibo(n - 2)
+
+
+while True:
+    try:
+        nbr = int(input("Enter a number : "))
+        if nbr == -1:
+            break
+        t = time.time()
+        iterfibo(nbr)
+        s = time.time() - t
+
+        print("IterFibo(%d) = %d, time %.6f" %(nbr, iterfibo(nbr), s))
+
+        t = time.time()
+        fibo(nbr)
+        s = time.time() - t
+
+        print("Fibo(%d) = %d, time %.6f" %(nbr, fibo(nbr), s))
+    except ValueError:
+        print("Enter number")
