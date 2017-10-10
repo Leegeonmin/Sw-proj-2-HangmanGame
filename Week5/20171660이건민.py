01536ed008696e8d9a78b9e92d09b2e462d731cc
LEE GEON MIN <lkm9709@kookmin.ac.kr>
2017-10-10 12:51:55 +0900

    fix list in

--- Week5/20171660이건민.py
+++ Week5/20171660이건민.py
@@ -6,20 +6,19 @@ import time
 
 ## fibo라는 리스트를 만들고
 ## fibo(0), fibo(1) 값을 0 , 1로 지정
-fibolist = []
-fibolist.append(0)
-fibolist.append(1)
+
 
 
 def iterfibo(n):
+    fibolist = []
+    fibolist.append(0)
+    fibolist.append(1)
     ## 피보나치 수열의 합을 저장할 sum = 0 지정
     sum = 0
     ## 피보나치 수열을 fibo리스트에 저장
-    for i in range(2, n):
+    for i in range(2, n + 1):
         fibolist.append(fibolist[i-1] + fibolist[i-2])
-    ## fibo리스트의 값들을 합
-    for p in range(n-2, n):
-        sum += fibolist[p]
+    sum = fibolist[n]
     return sum
 
 
