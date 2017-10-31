843d1ce79013623903c8a89d5291a7a58b052e81
LEE GEON MIN <lkm9709@kookmin.ac.kr>
2017-10-31 22:19:34 +0900

    upload fixing  assignment6

--- Week6/assignment6_skel.py
+++ Week6/assignment6_skel.py
@@ -20,59 +20,79 @@ class ScoreDB(QWidget):
 
         
     def initUI(self):
+        # define variable
         name = QLabel('Name:', self)
-        namelabel = QLineEdit(self)
+        self.namelabel = QLineEdit(self)
         age = QLabel('Age:', self)
-        agelabel = QLineEdit(self)
+        self.agelabel = QLineEdit(self)
         score = QLabel('Score:', self)
-        scorelabel = QLineEdit(self)
+        self.scorelabel = QLineEdit(self)
         amount = QLabel('Amount:', self)
-        amountlabel = QLineEdit(self)
+        self.amountlabel = QLineEdit(self)
         result = QLabel('Result:', self)
-        resultlabel = QTextEdit(self)
+        self.resultlabel = QTextEdit(self)
 
-
-        resultlabel.setGeometry(10,120,470,120)
-        namelabel.move(50,10)
-        agelabel.move(200, 10)
-        scorelabel.move(380, 10)
-        amountlabel.move(230, 50)
-        name.move(10, 10)
-        age.move(170, 10)
-        score.move(340, 10)
-        amount.move(170, 50)
-        result.move(10, 100)
-
-
-        addButton = QPushButton('Add',self)
-        addButton.move(100, 75)
-        delbutton = QPushButton('Del',self)
-        delbutton.move(180, 75)
-        findbutton = QPushButton('Find',self)
-        findbutton.move(260, 75)
-        incbutton = QPushButton('Inc',self)
-        incbutton.move(340, 75)
-        showbutton = QPushButton('Show', self)
-        showbutton.move(420, 75)
-
-        self.Key = QLabel('Key', self)
+        Key = QLabel('Key', self)
         combo = QComboBox(self)
         combo.addItem('Name')
         combo.addItem('Age')
         combo.addItem('Score')
-        combo.move(390, 50)
-        self.Key.move(350,50)
-        combo.activated[str].connect(self.onActivated)
+        combo.activated[str].connect(self.Del)
+        combo.activated[str].connect(self.Find)
+
+        addButton = QPushButton('Add',self)
+        delbutton = QPushButton('Del',self)
+        findbutton = QPushButton('Find',self)
+        incbutton = QPushButton('Inc',self)
+        showbutton = QPushButton('Show', self)
+
+        #set layout
+        hbox1 = QHBoxLayout()
+        hbox1.addWidget(name)
+        hbox1.addWidget(self.namelabel)
+        hbox1.addWidget(age)
+        hbox1.addWidget(self.agelabel)
+        hbox1.addWidget(score)
+        hbox1.addWidget(self.scorelabel)
+
+        hbox2 = QHBoxLayout()
+        hbox2.addWidget(amount)
+        hbox2.addWidget(self.amountlabel)
+        hbox2.addWidget(Key)
+        hbox2.addWidget(combo)
+
+        hbox3 = QHBoxLayout()
+        hbox3.addWidget(addButton)
+        hbox3.addWidget(delbutton)
+        hbox3.addWidget(findbutton)
+        hbox3.addWidget(incbutton)
+        hbox3.addWidget(showbutton)
+
+        hbox4 = QHBoxLayout()
+        hbox4.addWidget(result)
+
+        hbox5 = QHBoxLayout()
+        hbox5.addWidget(self.resultlabel)
+
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+        vbox.addLayout(hbox4)
+        vbox.addLayout(hbox5)
+        self.setLayout(vbox)
+
+        addButton.clicked.connect(self.Add)
+        showbutton.clicked.connect(self.Show)
+        delbutton.clicked.connect(self.Del)
+        findbutton.clicked.connect(self.Find)
+        incbutton.clicked.connect(self.Inc)
 
         self.setGeometry(300, 300, 500, 250)
-        self.setWindowTitle('Assignment6')    
+        self.setWindowTitle('Assignment6')
         self.show()
 
-        showbutton.clicked.connect(self.showScoreDB)
-
-    def onActivated(self, text):
-        self.Key.setText(text)
-        self.Key.adjustSize()
 
     def closeEvent(self, event):
 
@@ -88,23 +108,102 @@ class ScoreDB(QWidget):
         try:
             self.scoredb =  pickle.load(fH)
         except:
-            pass
+            print("Empty DB",self.dbfilename)
         else:
-            pass
+            # convert Age,Score to int type
+            print("Open DB", self.dbfilename)
+            for integer in self.scoredb:
+                integer['Age'] = int(integer['Age'])
+                integer['Score'] = int(integer['Score'])
+
+            return self.scoredb
         fH.close()
 
 
-    # write the data into person db
+    # convert Age,Score to int type
     def writeScoreDB(self):
         fH = open(self.dbfilename, 'wb')
+        for integer in self.scoredb:
+            integer['Age'] = int(integer['Age'])
+            integer['Score'] = int(integer['Score'])
+
         pickle.dump(self.scoredb, fH)
         fH.close()
 
-
+    # convert Age,Score to int type for make msg string
     def showScoreDB(self):
-        sortkey = self.Key
-        print(sortkey)
-        print(self.Key.text)
+        fh = self.readScoreDB()
+        msg = ''
+        for person in sorted(fh, key = lambda person : person['Name']):
+            for k , v in person.items():
+                    person[k] = str(person[k])
+                    k = str(k)
+                    msg += k + '=' + person[k] + '\t'
+
+            msg += '\n'
+        self.resultlabel.setText(msg)
+
+    def Add(self):
+        name = self.namelabel.text()
+        age = self.agelabel.text()
+        score = self.scorelabel.text()
+
+        addinfo = {'Name':name, 'Age':age, 'Score':score }
+        self.scoredb.append(addinfo)
+        self.writeScoreDB()
+        self.showScoreDB()
+    # make new list to del Del Name
+    def Del(self, text):
+        name = self.namelabel.text()
+        new_db = []
+
+        for i in self.scoredb:
+            if['Name'] != name:
+                new_db.append(i)
+
+        self.scoredb = new_db
+        self.writeScoreDB()
+        self.showScoreDB()
+    # Find same name and show infomation
+    def Find(self, text):
+        findname = self.namelabel.text()
+        msg = ''
+        new_db = []
+
+        for i in self.scoredb:
+            if i['Name'] == findname:
+                new_db.append(i)
+
+        for p in new_db:
+            for Key in p:
+                if Key == 'Score' or Key == 'Age':
+                    p[Key] = str(p[Key])
+                    msg += Key + '=' + p[Key] + '\t'
+
+            msg += '\n'
+        self.resultlabel.setText(msg)
+    # convert age, score to str type for make msg String
+    def Show(self):
+        info = self.combo.currentText()
+        msg = ''
+        for i in sorted(self.readScoreDB(), key=lambda person: person[info]):
+            for Key in sorted(i):
+                if Key == 'Age' or Key == 'Score':
+                    i[Key] = str(i[Key])
+                    Key = str(Key)
+                msg += Key + '=' + i[Key] + '\t'
+            msg += '\n'
+        self.resultlabel.setText(msg)
+    # Find same name and amount
+    def Inc(self):
+        name = self.namelabel.text()
+        amount = self.amountlabel.text()
+
+        for i in self.scoredb:
+            if i['Name'] == name:
+                i['Score'] += int(amount)
+        self.writeScoreDB()
+        self.showScoreDB()
 
 if __name__ == '__main__':
     
